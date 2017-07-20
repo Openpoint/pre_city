1f7e1c490cd6faa50f971119390a25e74cdbaabf
Michael Jonker <michael@openpoint.ie>
2017-07-20 16:04:37 +0100

    HTML cleanup and favicon

--- index.php
+++ index.php
@@ -1,33 +1,37 @@
 <!DOCTYPE html>
-<html ng-app='lkcity'>
+<html ng-app='lkcity' lang="en">
 
 	<head>
+		<meta charset="utf-8">
+		<link rel="shortcut icon" type="image/png" href="/images/favicon.png"/>
+		<title>CITY | internet GIS for the masses</title>
+		<meta name="description" content="Platform to present geographic data in consumable fashion">
+		<meta name="author" content="Openpoint">
 
-		<link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css">
-		<!-- Optional theme 
+		<!-- Optional theme
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">
 		-->
+		<link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css">
 		<link rel="stylesheet" href="/css/style.css">
-		
 		<link rel="stylesheet" href="/js/openlayers/css/ol.css">
 		<link rel="stylesheet" href="/css/mappopup.css">
 		<link rel="stylesheet" href="/css/gibson/stylesheet.css">
-		
+
 		<script src="/js/jquery-1.11.2.min.js"></script>
-		
+
 		<script src="/bootstrap/js/bootstrap.js"></script>
 		<script src="/js/openlayers/build/ol-debug.js"></script>
-		
+
 		<!--<script src="/js/angular.min.js"></script>-->
-		<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular.js"></script>		
-		<script src="/js/angular-sanitize.min.js"></script>	
+		<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular.js"></script>
+		<script src="/js/angular-sanitize.min.js"></script>
 		<script src="/js/uibootstrap/ui-bootstrap-custom-tpls-0.12.1.min.js"></script>
 		<script src="/js/angular-video/js/anguvideo.js"></script>
-		<!--	
+		<!--
 		<script src="/js/angular-openlayers-directive.js"></script>
 		-->
-		
-		
+
+
 	</head>
 	<body>
 		<div id='outer'  ng-controller='common'>
@@ -47,7 +51,7 @@
 		<script src="/controllers/unitstack.js"></script>
 		<script src="/controllers/leveldet.js"></script>
 		<script src="/controllers/edit.js"></script>
-		
+
 <!-- Piwik -->
 <script type="text/javascript">
   var _paq = _paq || [];
@@ -64,6 +68,6 @@
 <noscript><p><img src="//webstats.openpoint.ie/piwik.php?idsite=4" style="border:0;" alt="" /></p></noscript>
 <!-- End Piwik Code -->
 
-			
+
 	</body>
 </html>
